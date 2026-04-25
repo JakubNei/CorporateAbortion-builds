dd286a521702b2b7740a14d129897716fef69f99
Jakub Nei <nei.jakub@gmail.com>
2026-04-25 03:03:30 +0200

    Better first level

--- 1/Build/1.loader.js
+++ 1/Build/1.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 54628383,
+    wasmFileSize: 54631866,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
